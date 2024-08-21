0579d2878b4bbb451d05d7493502c0c9a9dd36db
AdityaWjy <adamfadzal662@gmail.com>
2024-08-21 12:33:45 +0700

    fix ui about

--- src/components/about/CardAbout.vue
+++ src/components/about/CardAbout.vue
@@ -5,28 +5,28 @@
     <div class="p-3 mb-2 bg-body-secondary rounded">
       <!-- create flex -->
       <div
-        class="d-flex flex-wrap flex-xl-row mt-2 mt-xl-4 align-items-stretch justify-content- gap-1 gap-xl-5"
+        class="d-flex flex-wrap flex-xl-row mt-2 mt-xl-4 align-items-stretch justify-content-between gap-1 gap-xl-5"
       >
         <div class="background-box shadow-lg mb-2 mb-xl-0">
           <div class="d-flex rounded h-100" style="background-color: #ffff">
-            <p class="text-dark p-3 text-content pt-5">
+            <p class="text-dark px-3 text-content pt-3 pt-xl-5 mt-xl-2">
               <span class="fw-bold"> Mamysehat </span>
-              <span class="fw-bold blueOcean"> Academy </span> ini menawarkan
-              berbagai program pelatihan klinis, seminar, workshop, dan webinar.
-              Program-program ini dirancang untuk memenuhi kebutuhan beragam
-              praktisi kesehatan, mulai dari perawat, bidan, dokter, hingga
-              tenaga medis lainnya.
+              <span class="fw-bold blueOcean"> Academy </span>
+              ini menawarkan berbagai program pelatihan klinis, seminar,
+              workshop, dan webinar. Program-program ini dirancang untuk
+              memenuhi kebutuhan beragam praktisi kesehatan, mulai dari perawat,
+              bidan, dokter, hingga tenaga medis lainnya.
             </p>
           </div>
         </div>
 
         <div class="background-box shadow-lg mb-2 mb-xl-0">
           <div class="d-flex rounded h-100" style="background-color: #ffff">
-            <p class="text-dark p-3 text-content pt-5">
+            <p class="text-dark px-3 text-content pt-3 pt-xl-5 pb-xl-4">
               <span class="fw-bold"> Mamysehat </span>
-              <span class="fw-bold blueOcean"> Academy </span> mengutamakan
-              kualitas pengajaran dengan mengundang para ahli dan praktisi
-              terkemuka dalam bidangnya sebagai instruktur. Mereka juga
+              <span class="fw-bold blueOcean"> Academy </span>
+              mengutamakan kualitas pengajaran dengan mengundang para ahli dan
+              praktisi terkemuka dalam bidangnya sebagai instruktur. Mereka juga
               menggunakan teknologi terbaru dalam metode pengajaran untuk
               memastikan pengalaman belajar yang interaktif dan efektif.
             </p>
@@ -35,7 +35,7 @@
 
         <div class="background-box shadow-lg mb-2 mb-xl-0">
           <div class="d-flex rounded h-100" style="background-color: #ffff">
-            <p class="text-dark p-3 text-content pt-5">
+            <p class="text-dark px-3 text-content pt-3 pt-xl-5 pb-xl-4">
               <span class="fw-bold"> Mamysehat </span>
               <span class="fw-bold blueOcean"> Academy </span> Melalui pelatihan
               yang di berikan, Mamysehat Academy berharap dapat memberikan
@@ -56,6 +56,7 @@ export default {};
 .text-content {
   width: 800px;
   font-size: 15px;
+  text-align: justify;
 }
 
 img {
@@ -64,14 +65,14 @@ img {
 }
 
 .background-box {
-  width: 500px;
+  width: 600px;
 }
 
 @media (max-width: 500px) {
   .text-content {
     width: auto;
-    text-align: justify;
     font-size: 10px;
+    text-align: justify;
   }
 
   h1 {
